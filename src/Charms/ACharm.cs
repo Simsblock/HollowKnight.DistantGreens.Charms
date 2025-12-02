ee115765da273a7b789eb3f890a01b00da376446
Simsblock <146435639+Simsblock@users.noreply.github.com>
2025-12-03 00:10:28 +0100

    MossMask Charm Behaviour

---
+++ src/Charms/ACharm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DistantGreensCharms.Charms;
+
+public abstract class ACharm
+{
+    public abstract string Sprite { get; }
+    public abstract string Name { get; }
+    public abstract string Description { get; }
+    public abstract int DefaultCost { get; }
+    public abstract string Scene { get; }
+    public abstract float X { get; } //Coordinates in Scene
+    public abstract float Y { get; } //Coordinates in Scene
+    
+    public CharmState State { get; set; }
+    
+    // assigned at runtime by SFCore's CharmHelper
+    public int Num { get; set; }
+    
+    public bool Equipped() => PlayerData.instance.GetBool($"equippedCharm_{Num}");
+    
+    public virtual void Hook() {}
+    public virtual List<(string obj, string fsm, Action<PlayMakerFSM> edit)> FsmEdits => new(); //List for all FSM Edits a Charm needs to do, applied at Runtime
+    public virtual List<(int Period, Action Func)> Tickers => new(); //List for all Function Calls which need to executed reguarly, 
+}
+
+public struct CharmState
+{
+    public bool Obtained;
+    public bool Equipped;
+    public bool New; // Newly aquired for New Notification in Inventory
+    public int Cost; // Notches
+}
---
+++ src/Charms/MossMask.cs
@@ -0,0 +1,38 @@
+﻿using ItemChanger.Items;
+using Modding;
+
+namespace DistantGreensCharms.Charms;
+
+public class MossMask : ACharm
+{
+    public static readonly MossMask Instance = new();
+    
+    public override string Sprite  => "CharmIcons/MossMask.png"; //test
+    public override string Name => "Moss-Mask"; 
+    public override string Description => "Moss-Mask description"; //todo
+    public override int DefaultCost => 3;
+    public override string Scene => "GG_Workshop"; //todo
+    public override float X => 17.65f; //todo
+    public override float Y => 6.41f; //todo
+
+    public override void Hook()
+    {
+        ModHooks.TakeHealthHook += CheckMaskActivation;
+        ModHooks.SetPlayerBoolHook += ChargeCharm;
+    }
+
+    private bool charged = true;
+    private int CheckMaskActivation(int damage)
+    {
+        if (!Equipped() || !charged || PlayerData.instance.health - damage > 0) return damage;
+        if (PlayerData.instance.health - damage > 0) return damage;
+        charged = false;
+        return 0;
+    }
+
+    private bool ChargeCharm(string targetBool, bool value)
+    {
+        if(targetBool == "atBench" && value && Equipped()) charged = true;
+        return value;
+    }
+}

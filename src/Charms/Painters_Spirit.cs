05ab60d9a42899320c94cb54699f2e43ff584902
Simsblock <146435639+Simsblock@users.noreply.github.com>
2025-12-13 00:12:57 +0100

    Improved Upon Painters Spirit, still not on Nail Art

--- src/Charms/Painters_Spirit.cs
+++ src/Charms/Painters_Spirit.cs
@@ -1,11 +1,19 @@
-﻿using DistantGreensCharms.Helper;
+﻿using System;
+using System.Collections.Generic;
+using DebugMod;
+using DistantGreensCharms.Helper;
 using DistantGreensCharms.Settings;
 using GlobalEnums;
 using HutongGames.PlayMaker.Actions;
+using ItemChanger.Locations.SpecialLocations;
 using Modding;
 using MonoMod.RuntimeDetour;
+using RandomizerCore.Extensions;
+using SFCore.Utils;
 using UnityEngine;
 using UnityEngine.Experimental.TerrainAPI;
+using Bounds = UnityEngine.Bounds;
+using Random = System.Random;
 
 namespace DistantGreensCharms.Charms;
 
@@ -29,41 +37,16 @@ public class Painters_Spirit : ACharm
     public override void Hook()
     {
         Create_paintDecalPrefab();
-
+        
         On.NailSlash.StartSlash += OnNailSlash;
         //ModHooks.AttackHook += OnNailSlash;
-
-    }
-
-    private GameObject _paintDecalPrefab;
-
-    private void Create_paintDecalPrefab()
-    {
-        DistantGreensCharms.Instance.Log("Creating paint decal prefab...");
-
-        _paintDecalPrefab = new GameObject("PaintDecal");
-        DistantGreensCharms.Instance.Log($"Created GameObject: {_paintDecalPrefab != null}");
-
-        var renderer = _paintDecalPrefab.AddComponent<SpriteRenderer>();
-        DistantGreensCharms.Instance.Log($"Added SpriteRenderer: {renderer != null}");
-
-        Sprite sprite = SpriteManager.Get("paint");
-        DistantGreensCharms.Instance.Log($"Created Sprite: {sprite != null}");
-
-        renderer.sprite = sprite;
-        renderer.sortingLayerName = "Default";
-        renderer.sortingOrder = 10;
-        
-        GameObject.DontDestroyOnLoad(_paintDecalPrefab);
-        DistantGreensCharms.Instance.Log($"Marked DontDestroyOnLoad");
-
-        _paintDecalPrefab.SetActive(false);
-        DistantGreensCharms.Instance.Log($"Set inactive. Prefab null? {_paintDecalPrefab == null}");
     }
 
     private void OnNailSlash(On.NailSlash.orig_StartSlash orig, global::NailSlash self)
     {
         orig(self);
+        
+        DistantGreensCharms.Instance.Log("nailslashnames: " + self.name + " " + self.animName + " " + self.gameObject.name);
 
         DistantGreensCharms.Instance.Log($"OnNailSlash called. Prefab null? {_paintDecalPrefab == null}");
 
@@ -76,56 +59,62 @@ public class Painters_Spirit : ACharm
         SpawnPaintOnGround(self.transform.position);
     }
 
+    private GameObject _paintDecalPrefab;
+    private Sprite[] _paintDecalSprites =
+        [SpriteManager.Get("Misc.paint0"), SpriteManager.Get("Misc.paint1"), SpriteManager.Get("Misc.paint2"),SpriteManager.Get("Misc.paint3"),SpriteManager.Get("Misc.paint4")];
+    private Random _random = new();
+    private void Create_paintDecalPrefab()
+    {
+        DistantGreensCharms.Instance.Log("Creating paint decal prefab...");
+
+        _paintDecalPrefab = new GameObject("PaintDecal");
+        DistantGreensCharms.Instance.Log($"Created GameObject: {_paintDecalPrefab != null}");
+
+        var renderer = _paintDecalPrefab.AddComponent<SpriteRenderer>();
+        DistantGreensCharms.Instance.Log($"Added SpriteRenderer: {renderer != null}");
+        
+        renderer.sortingLayerName = "Default";
+        
+        GameObject.DontDestroyOnLoad(_paintDecalPrefab);
+        DistantGreensCharms.Instance.Log($"Marked DontDestroyOnLoad");
+
+        _paintDecalPrefab.SetActive(false);
+        DistantGreensCharms.Instance.Log($"Set inactive. Prefab null? {_paintDecalPrefab == null}");
+    }
+    
     private void SpawnPaintOnGround(Vector3 attackPosition)
     {
         if (!Equipped()) return;
-        DistantGreensCharms.Instance.Log($"SpawnPaintOnGround called. Prefab null? {_paintDecalPrefab == null}");
-
         RaycastHit2D hit = Physics2D.Raycast(
             attackPosition,
             Vector2.down,
             5f,
             LayerMask.GetMask("Terrain")
         );
-
-        DistantGreensCharms.Instance.Log($"Raycast hit: {hit.collider != null}");
-
+        //DistantGreensCharms.Instance.Log($"Raycast hit: {hit.collider.gameObject.name}");
+        
         if (hit.collider != null && hit.collider.gameObject.tag == "Untagged")
         {
-            //DistantGreensCharms.Instance.Log($"Hit object: {hit.collider.gameObject.name}");
-            //DistantGreensCharms.Instance.Log($"Layer index: {hit.collider.gameObject.layer}");
-            //DistantGreensCharms.Instance.Log($"Layer name: {LayerMask.LayerToName(hit.collider.gameObject.layer)}");
-            //DistantGreensCharms.Instance.Log($"Tag: {hit.collider.gameObject.tag}");
-
-            //DistantGreensCharms.Instance.Log($"Hit point: {hit.point}");
-            //DistantGreensCharms.Instance.Log($"Hit normal: {hit.normal}");
-            
-            GameObject ground = hit.collider.gameObject;
-            //DistantGreensCharms.Instance.Log($"Hit ground: {ground.name}");
-            
-            var meshRenderer = ground.GetComponent<MeshRenderer>();
-            //if (meshRenderer != null)
-            //{
-            //    DistantGreensCharms.Instance.Log("Ground uses MeshRenderer!");
-            //} 
-            
-            var layer = ground.layer;
-            //DistantGreensCharms.Instance.Log("Ground uses Layer: "+layer.ToString());
-
-            //DistantGreensCharms.Instance.Log($"About to instantiate. Prefab null? {_paintDecalPrefab == null}");
-
-            // This is the line that's throwing the error
                 GameObject decal = GameObject.Instantiate(_paintDecalPrefab);
-
-                DistantGreensCharms.Instance.Log("Instantiated successfully!");
-
-                decal.transform.position = new Vector3(
+                Vector3 paintPosition = new Vector3(
                     hit.point.x,
-                    hit.point.y - 0.64f //,
-                    //groundRenderer.transform.position.z
+                    hit.point.y - 0.64f,
+                    hit.transform.GetPositionZ() - 0.5f - (_random.Next(10)/100f) // to randomize if the new paint is above or below a previous one
                 );
-                //decal.transform.rotation = Quaternion.identity;
-                //decal.transform.Rotate(0, 0, Random.Range(0f, 360f));
+                Bounds hitBounds = hit.collider.bounds;
+                List<Sprite> possibleSprites = new();
+                foreach (var sprite in _paintDecalSprites) //maybe check y-axis too todo
+                {
+                    float height = sprite.bounds.size.y;
+                    float width = sprite.bounds.size.x;
+                    float halfWidth = width / 2f;
+                    if(paintPosition.x +  halfWidth <= hitBounds.max.x &&
+                       paintPosition.x -  halfWidth >= hitBounds.min.x) possibleSprites.Add(sprite);
+                }
+
+                if (possibleSprites.Count == 0) return;
+                decal.GetComponent<SpriteRenderer>().sprite = possibleSprites[_random.Next(possibleSprites.Count)];
+                decal.transform.position = paintPosition;
                 decal.SetActive(true);
         }
     }
